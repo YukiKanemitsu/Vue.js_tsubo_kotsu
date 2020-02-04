61509d5b7e7c35800681fb6fd4037826c9fd192a
y-kanemitsu <y-kanemitsu@topknock.co.jp>
2020-02-04 17:44:30 +0900

    Add_イベントハンドリング

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
 var app = new Vue({
     el: "#app",
     data: {
-        message: "Hello Vue!"
+        point: { 
+            x: 0,
+            y: 0,
+        },
+        //ウィンドウサイズ
+        width: window.innerWidth,
+        height: window.innerHeight,
+        stock: 10
+    },
+    created: function(){
+        //イベントハンドラを登録
+
+        addEventListener('mousemove', this.mousemoveHandler);
+        addEventListener('resize', this.resizeHandler);
+    },
+    beforeDestroy: function() {
+        //イベントハンドラを解除
+
+        removeEventListener('mousemove', this.mousemoveHandler);
+        removeEventListener('resize', this.resizeHandler);
+    },
+    methods: {
+        // mousemoveイベントハンドラ
+        mousemoveHandler: function($event) {
+            this.point.x = $event.clientX;
+            this.point.y = $event.clientY;
+        },
+
+        // イベントハンドラ
+        resizeHandler: function ($event) {
+            // 現在のウィンドウサイズでプロパティを更新
+            this.width = $event.target.innerWidth;
+            this.height = $event.target.innerHeight;
+        },
+        //削除ボタンのクリックイベントハンドラ
+        onDeleteItem: function () {
+            this.stock--;
+        }
     }
 });
