0d1c4ddf280248cae826758a953bb5a3d9665e9d
y-kanemitsu <y-kanemitsu@topknock.co.jp>
2020-02-11 01:21:40 +0900

    Add_3-5ユーザーの入力に応じて表示を切り替える

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ Vue.filter('number_format', function (val) {
 var app = new Vue({
     el: '#app',
     data: {
-        // 表示中の商品数
-        count: 0,
         // 「セール対象」のチェック状態(true:チェックあり、false:チェック無し)
         showSaleItem: false,
         // 「送料無料」のチェック状態(true:チェックあり、false:チェック無し)
@@ -59,5 +57,34 @@ var app = new Vue({
                 isSale: false,
             },
         ]
+    },
+    computed: {
+        // 絞り込み後の商品リストを返す算出プロパティ
+        filteredList: function () {
+            // 絞り込み後の商品リスト格納する新しい配列
+            var newList = [];
+            for (var i = 0; i < this.products.length; i++) {
+                // 表示対象かどうかを判定する
+                var isShow = true;
+                // 1番目の商品が表示対象かどうかを判定する
+                if (this.showSaleItem && !this.products[i].isSale) {
+                    // 「セール対象」チェック有りで、セール対象ではない場合
+                    isShow = false; //この商品は表示しない
+                }
+                if (this.showDelvFree && this.products[i].delv > 0) {
+                    // 「送料無料」チェック有りで、送料有りの商品の場合
+                    isShow = false;
+                }
+                // 表示対象の商品だけを新しい配列に追加する
+                if (isShow) {
+                    newList.push(this.products[i]);
+                }
+            }
+            // 絞り込み後の商品リストを返す
+            return newList;
+        },
+        count: function () {
+           return this.filteredList.length; 
+        }
     }
 });
