a9f523b210f7697f4902c99e991d070c51e05cda
y-kanemitsu <y-kanemitsu@topknock.co.jp>
2020-03-29 14:30:11 +0900

    [4-4]JSONPでクロスドメイン制約を回避する

--- main.js
+++ main.js
@@ -1,39 +1,21 @@
-// var btnLoad = document.querySelector('#load');
-// // 読み込み用のイベントハンドラを定義
-// btnLoad.addEventListener('click', function (event) {
-//     // 【手順1】XMLHttpRequestオブジェクトのインスタンスを生成
-//     var xmlHttpRequest = new XMLHttpRequest();
-//     // 【手順2】 通信状態の変化を監視するイベントハンドラを設定
-//     xmlHttpRequest.onreadystatechange = function () {
-//         // レスポンスの受信が正常に完了した時
-//         if (this.readyState == 4 /* && this.status == 200 */ ) {
-//             // 受信したデータをコンソールに出力する
-//             console.log(this.readyState, this.response);
-//         }
-//     };
-//     // 【手順3】レスポンスの形式を指定する
-//     xmlHttpRequest.responseType = 'json';
-//     // 【手順4】リクエストメソッドと読み込むファイルのパスを指定する
-//     xmlHttpRequest.open('GET', 'products.json');
-//     // 【手順5】リクエストを送信する(非同期通信を開始する)
-//     xmlHttpRequest.send();
-// });
+// JSONPのURL(サーバーに配置する)
+var url = 'サーバーのURL/products.js';
 
-$('#load').on('click', clickHandler);
-function clickHandler(event) {
-    // 非同期通信でJSONを読み込む
-    $.ajax({
-        url: 'products.json',
-        type: 'GET',
-        dataType: 'json'
-    })
-    .done(function (data, textStatus, jqXHR) {
-        updateScreen(data);
-    })
-    .fail(function (jqXHR, textStatus, errorThrown) {
-        console.log('通信が失敗しました');
-    });
-}
+// 非同期通信でJSONPを読み込む
+$.ajax({
+    url: url,                   //通信先URL
+    type: 'GET',                //使用するHTTPメソッド
+    dataType: 'jsonp',          //レスポンスのデータタイプ
+    jsonp: 'callback',          //クエリパラメータの名前
+    jsonpCallback: 'products'   //コールバック関数の名前
+})
+.done(function (data, textStatus, jqXHR) {
+    console.log(data);
+})
+.fail(function (jqXHR, textStatus, errorThrown) {
+    console.log('通信が失敗しました');
+});
+
 
 // 商品一覧の描画を更新する
 function updateScreen(products) {
