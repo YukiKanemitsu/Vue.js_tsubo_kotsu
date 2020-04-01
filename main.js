682c69dcc0e9ae075e822bccc13e0bff08de289e
YukiKanemitsu <y.kanemitsu0723@gmail.com>
2020-04-02 01:37:03 +0900

    [Edit]5-2モックアップの作成完成

--- main.js
+++ main.js
@@ -96,14 +96,82 @@ function number_format(val) {
     return val.toLocaleString();
 }
 
+// 日付の差を求める関数
+function getDateDiff(dataString1, dataString2) {
+    // 日付を表す文字列から日付オブジェクトを生成
+    const date1 = new Date(dataString1); 
+    const date2 = new Date(dataString2);
+    // ２つの日付の差分(ミリ秒)を計算
+    const msDiff = date1.getTime() - date2.getTime();
+    // 求めた差分(ミリ秒)を日付に変換
+    // 差分÷(1000ミリ秒×60秒×60分×24時間)
+    return Math.cail(msDiff / (1000 * 60 * 60 * 24));
+}
+
 // 再計算した基本料金(税込)を返す関数
 function taxedBasePrice() {
+    // 割増料金
+    var addPrice = 0;
+    // フォームコントロールを取得(DVD仕上がり予定日)
+    var delivery_date = app.querySelector('#delivery_date');
+    // 納期までの残り日数を計算
+    var deteDiff = getDateDiff(delivery_date.value, (new Date()).toLocaleString());
+
+    // 割増料金を求める
+    if (21 <= deteDiff && dateDiff < 30) {
+        // 納期が1ヶ月未満の場合
+        addPrice = 5000;
+    } else if (14 <= dateDiff && dateDiff < 21) {
+        // 納期が3週間未満の場合
+        addPrice = 10000;
+    } else if (7 <= dateDiff && dateDiff < 14) {
+        // 納期が2週間未満の場合
+        addPrice = 10000;
+    } else if (3 <= dateDiff && dateDiff < 7) {
+        // 納期が1週間未満の場合
+        addPrice = 10000;
+    } else if (dateDiff == 3) {
+        // 納期が3日後の場合
+        addPrice = 10000;
+    } else if (dateDiff == 2) {
+        // 納期が2日後の場合
+        addPrice = 10000;
+    } else if (dateDiff == 1) {
+        // 納期が翌日の場合
+        addPrice = 10000;
+    } 
     // 基本料金(税込)を返す
+    return incTax(30000 + addPrice);
 }
 
 // 再計算したオプション料金(税込)を返す関数
 function taxedOptPrice() {
+    // オプション料金
+    var optPrice = 0;
+    // フォームコントロールを取得
+    var opt1 = app.querySelector('#opt1'); //BGM手配
+    var opt2 = app.querySelector('#opt2'); //撮影
+    var opt3 = app.querySelector('#opt3'); //DVD盤面印刷
+    var opt4 = app.querySelector('#opt4'); //写真スキャニング
+    // BGM手配
+    if (opt1.checked) {
+        optPrice += 5000;
+    }
+    // 撮影
+    if (opt2.checked) {
+        optPrice += 5000;        
+    }
+    // DVD盤面印刷
+    if (opt3.checked) {
+        optPrice += 5000;        
+    }
+    // 写真スキャニング
+    if (opt4.value == '') {
+        opt4 = 0;       
+    }
+    optPrice += opt4.value * 500;
     // オプション料金(税込)を返す
+    return incTax(optPrice);
 }
 
 // 金額の表示を更新する関数
@@ -114,9 +182,12 @@ function updateForm() {
     var sum_total = app.querySelector('#sum_total'); //合計(税込)
 
     // 金額を再計算
-    var basePrice = taxedBasePrice();
-    var optPrice = taxedOptPrice();
-    var totalPrice = basePrice + optPrice;
+    var basePrice = taxedBasePrice(); //基本料金(税込)
+    var optPrice = taxedOptPrice(); //オプション料金(税込)
+    var totalPrice = basePrice + optPrice; //合計(税込)
 
     // 表示を更新
+    sum_base.value = number_format(basePrice); //基本料金(税込)
+    sum_opt.value = number_format(optPrice); //オプション料金(税込)
+    sum_total.value = number_format(totalPrice); //合計(税込)
 }
