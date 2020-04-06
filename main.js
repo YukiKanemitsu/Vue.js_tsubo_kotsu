7561270e1cc9398a4d3cc2237ec0fe6cedf12f00
YukiKanemitsu <y.kanemitsu0723@gmail.com>
2020-04-07 00:15:24 +0900

    [Edit]is属性

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 const app = new Vue({
     el: '#app',
     data: {
-        price: 980
+        products: [
+            {
+                id: 1,
+                name: '商品A',
+                price: 1280
+            },
+            {
+                id: 2,
+                name: '商品B',
+                price: 1580
+            },
+            {
+                id: 3,
+                name: '商品C',
+                price: 1980
+            },
+        ]
     },
     methods: {
-        // (5)子から呼び出されるメソッド
-        priceDown: function () {
-            var discount = 0;
-            // 値下げ幅が指定されていない場合は100円とする
-            if (this.price - 50 < 500) {
-                // 例)現在の価格が530円の場合、値下げ幅は30円
-                discount = this.price - 500;
-            } else {
-                // 例)現在の価格が550円以上の場合、値下げ幅は50円
-                discount = 50;
-            }
-            // 値下げする
-            this.price -= discount;
-        }
+
     }
 });
--- my-product.js
+++ my-product.js
@@ -1,11 +1,21 @@
-Vue.component('my-product', {
+Vue.component('my-row', {
     // (1)ボタンがクリックされたら子コンポーネントの「clickHandler」を呼び出す
     template: `
-            <button>
-                現在の価格　{{ price }}(円)
-            </button>`
+        <tr>
+            <td>
+                {{ id }} 
+            </td>
+            <td>
+                {{ name }} 
+            </td>
+            <td>
+                {{ price }} (円)
+            </td>
+        </tr>`
     ,
     props: [
+        'id',
+        'name',
         'price'
     ],
 });
